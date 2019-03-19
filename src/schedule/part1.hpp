f4cf5c15b12fe0dbc72a1e0c46612688c1e18d39
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-19 15:14:19 -0500

    read processes from file on all schedulers

--- src/schedule/part1.hpp
+++ src/schedule/part1.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <vector>
+
+#include "process.hpp"
+#include "cpu.hpp"
+
 void schedule_Part1()
 {
-
+    std::vector<Process> processes;
+    Process::read_from_stdin(processes);
 }
 
