7716aa29a0402236cd9c296bf593380856ab9fdd
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-19 14:48:20 -0500

    add framework for running each part

---
+++ src/schedule.cpp
@@ -0,0 +1,22 @@
+#include "schedule.hpp"
+
+void schedule_Part1()
+{
+    
+}
+
+void schedule_Part2()
+{
+     
+}
+
+void schedule_Part3()
+{
+    
+}
+
+void schedule_Part4()
+{
+    
+}
+
---
+++ src/schedule.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+void schedule_Part1();
+void schedule_Part2();
+void schedule_Part3();
+void schedule_Part4();
+
